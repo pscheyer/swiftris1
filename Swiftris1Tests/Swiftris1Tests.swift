b4764b154a0926536c1baa21941ad7a252fb467e
Peter Scheyer <pscheyer@gmail.com>
2015-04-25 22:05:23 +0300

    Initial Commit

---
+++ Swiftris1Tests/Swiftris1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Swiftris1Tests.swift
+//  Swiftris1Tests
+//
+//  Created by Peter Scheyer on 4/25/15.
+//  Copyright (c) 2015 Peter Scheyer. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Swiftris1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
